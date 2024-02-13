700aca24407ffc0f01d1a7d3bd1144f6a2db1247
reduanhaiderrifat <reduanhaiderrifat@gmail.com>
2024-02-13 11:34:40 +0600

    my last commit

--- script.js
+++ script.js
@@ -38,18 +38,86 @@ function calculatorRectangleArea(){
     const rectangleArea = document.getElementById('rectangle-area');
     rectangleArea.innerText = area;
 }
-function calculatorParallelogramArea(){
-    const rectangleBaseInput = document.getElementById('parallelogram-base');
-    const BaseNumber = rectangleBaseInput.value;
-    const base = parseFloat(BaseNumber);
-    console.log(base)
-    const rectangleHeightInput = document.getElementById('parallelogram-height');
-    const rectangleLengthNumber = rectangleHeightInput.value;
-    const height = parseFloat(rectangleLengthNumber);
-    console.log(height);
+// function calculatorParallelogramArea(){
+//     const rectangleBaseInput = document.getElementById('parallelogram-base');
+//     const BaseNumber = rectangleBaseInput.value;
+//     const base = parseFloat(BaseNumber);
+//     console.log(base)
+//     const rectangleHeightInput = document.getElementById('parallelogram-height');
+//     const rectangleLengthNumber = rectangleHeightInput.value;
+//     const height = parseFloat(rectangleLengthNumber);
+//     console.log(height);
 
+//     const area = base * height;
+//     console.log(area);
+//     const parallelogramArea = document.getElementById('parallelogram-area');
+//    parallelogramArea.innerText = area;
+// }
+
+function calculatorParallelogramArea(){
+    const base = getInputValueById('parallelogram-base');
+    
+    const height = getInputValueById('parallelogram-height')
+    
     const area = base * height;
     console.log(area);
     const parallelogramArea = document.getElementById('parallelogram-area');
-   parallelogramArea.innerText = area;
+    parallelogramArea.innerText = area;
+  
+}
+
+function getInputValueById(input){
+    const inputfeild = document.getElementById(input)
+  const inputNumber = inputfeild.value;
+  const inputValue = parseFloat(inputNumber);
+  return inputValue;
+
+}
+
+function getRhombusvalue(input){
+    const inputfeild = document.getElementById(input);
+    const inputNumber = inputfeild.value;
+    const inputValue = parseFloat(inputNumber);
+    return inputValue;
+
+}
+
+function caculatorRhombusArea(){
+    const d1 = getRhombusvalue('rhombus-d1');
+    const d2 = getRhombusvalue('rhombus-d2');
+    const area = (d1*d2)/2
+    console.log(area);
+    const rhombusArea = document.getElementById('rhombus-area');
+    rhombusArea.innerText = area;
+}
+function getPentagonvalue(input){
+    const inputfeild = document.getElementById(input);
+    const inputNumber = inputfeild.value;
+    const inputValue = parseFloat(inputNumber);
+    return inputValue;
+
+}
+function caculatorPentagonArea(){
+    const p = getPentagonvalue('pentagon-p');
+    const b = getPentagonvalue('pentagon-b');
+    const area = 0.5 * p * b;
+    console.log(area);
+    const pentagonArea = document.getElementById('pentagon-area');
+    pentagonArea.innerText = area;
+
+}
+function getEllipsevalue(input){
+    const inputfeild = document.getElementById(input);
+    const inputNumber = inputfeild.value;
+    const inputValue = parseFloat(inputNumber);
+    return inputValue;
+
+}
+function caculatorEllipseArea(){
+    const a = getEllipsevalue('ellipse-a');
+    const b = getEllipsevalue('ellipse-b');
+    const area = 3.1415 * a * b;
+    console.log(area);
+    const ellipseArea = document.getElementById('ellipse-area');
+    ellipseArea.innerText = area;
 }
